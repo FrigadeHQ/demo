17777a95289830dcd98897c32c9b868143ecb71d
Eric Brownrout <ericbrownrout@gmail.com>
2024-08-13 16:25:29 -0700

    styling form progress

--- src/app/forms/page.tsx
+++ src/app/forms/page.tsx
@@ -5,6 +5,7 @@ import { useFlow, useUser } from '@frigade/react';
 import { Progress } from '@/components/ui/progress';
 import { Card } from '@/components/ui/card';
 import { Spinner } from '@/components/spinner';
+import { Button } from '@/components/ui/button';
 
 export default function Forms() {
   const ONBOARDING_FORM_FLOW_ID = 'flow_kTB2fci9';
@@ -20,57 +21,62 @@ export default function Forms() {
         <div className="col-span-4">
           <Card className="md:w-[500px] w-full flex flex-col justify-center mx-auto shadow-sm pb-5 overflow-hidden">
             <Progress value={progress} className="rounded-none" />
-            <div className="flex flex-col justify-center space-y-6 w-full min-h-[300px] px-5">
-              {/*<Text.Body2 fontWeight="demibold">Step 1 of 3</Text.Body2>*/}
+            <div className="flex flex-col justify-center space-y-6 w-full px-5">
               {flow?.isCompleted || !flow ? (
                 <div className="flex items-center justify-center w-full h-full">
                   <Spinner />
                 </div>
               ) : (
-                <Frigade.Form
-                  flowId={ONBOARDING_FORM_FLOW_ID}
-                  dismissible={false}
-                  className="onboarding-form"
-                  css={{
-                    '.fr-button-primary': {
-                      width: '100%',
-                    },
-                    '.fr-field-label': {
-                      fontSize: '14px',
-                      marginBottom: '14px',
-                      lineHeight: '22px',
-                    },
-                    '.fr-field-check-value, .fr-field-radio-value': {
-                      width: '20px',
-                      height: '20px',
-                    },
-                    '.fr-field-check-label, .fr-field-radio-label': {
-                      lineHeight: '20px',
-                    },
-                    '.fr-field-input, .fr-field-select': {
-                      height: '38px',
-                    },
-                    '.fr-field-radio:hover, .fr-field-check:hover': {
-                      borderColor: 'var(--fr-colors-primary-background)',
-                    },
-                    '.fr-field-label-required': {
-                      display: 'none',
-                    },
-                    '.fr-field-select-icon': {
-                      color: 'var(--fr-colors-neutral-foreground)',
-                    },
-                    '.fr-field-radio': {
-                      cursor: 'pointer',
-                    },
-                    '.fr-field-check, .fr-field-radio': {
-                      height: '38px',
-                    },
-                    // FIXME/HELPME: @micah for some reason this doesn't work.
-                    // '.fr-field-select-option:hover': {
-                    //   backgroundColor: 'var(--fr-colors-secondary-hover-background)',
-                    // },
-                  }}
-                />
+                <div className="flex-col justify-center w-full">
+                  <Frigade.Form
+                    flowId={ONBOARDING_FORM_FLOW_ID}
+                    className="onboarding-form pt-12 pb-4"
+                    css={{
+                      '.fr-button-primary': {
+                        width: '100%',
+                      },
+                      '.fr-field-label': {
+                        fontSize: '14px',
+                        marginBottom: '14px',
+                        lineHeight: '22px',
+                      },
+                      '.fr-field-check-value, .fr-field-radio-value': {
+                        width: '20px',
+                        height: '20px',
+                      },
+                      '.fr-field-check-label, .fr-field-radio-label': {
+                        lineHeight: '20px',
+                      },
+                      '.fr-field-input, .fr-field-select': {
+                        height: '38px',
+                      },
+                      '.fr-field-radio:hover, .fr-field-check:hover': {
+                        borderColor: 'var(--fr-colors-primary-background)',
+                      },
+                      '.fr-field-label-required': {
+                        display: 'none',
+                      },
+                      '.fr-field-select-icon': {
+                        color: 'var(--fr-colors-neutral-foreground)',
+                      },
+                      '.fr-field-radio': {
+                        cursor: 'pointer',
+                      },
+                      '.fr-field-check, .fr-field-radio': {
+                        height: '38px',
+                      },
+                      // FIXME/HELPME: @micah for some reason this doesn't work.
+                      // '.fr-field-select-option:hover': {
+                      //   backgroundColor: 'var(--fr-colors-secondary-hover-background)',
+                      // },
+                    }}
+                  />
+                  <Button variant="link">
+                    <p className="text-sm text-muted-foreground">
+                      Skip for now
+                    </p>
+                  </Button>
+                </div>
               )}
             </div>
             <style>
