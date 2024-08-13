6bcb7ca6520533ffac4280617d7eb3c08312e0a0
Eric Brownrout <ericbrownrout@gmail.com>
2024-08-13 15:40:42 -0700

    broken push - form styling start

--- src/app/forms/page.tsx
+++ src/app/forms/page.tsx
@@ -1,9 +1,99 @@
 import * as Frigade from '@frigade/react';
+import { useFlow, useUser } from '@frigade/react';
+import { Progress } from '@/components/ui/progress';
+import { Card } from '@/components/ui/card';
+import { Spinner } from '@/components/spinner';
 
 export default function Forms() {
+  const ONBOARDING_FORM_FLOW_ID = 'flow_kTB2fci9';
+  const { flow } = useFlow(ONBOARDING_FORM_FLOW_ID);
+  const { addProperties } = useUser();
+  const progress =
+    ((flow?.getNumberOfCompletedSteps() || 0) /
+      (flow?.getNumberOfAvailableSteps() || 1)) *
+    100;
   return (
     <>
-      <Frigade.Form flowId="flow_kTB2fci9" />
+      <div
+        className="container relative flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-4 lg:px-0 overflow-y-scroll"
+        style={{
+          height: 'calc(100vh - 70px)',
+          paddingBottom: '70px',
+        }}
+      >
+        <div className="col-span-4">
+          <Card className="md:w-[500px] w-full flex flex-col justify-center mx-auto shadow-sm pb-5 overflow-hidden">
+            <Progress value={progress} className="rounded-none" />
+            <div className="flex flex-col justify-center space-y-6 w-full min-h-[300px] px-5">
+              {/*<Text.Body2 fontWeight="demibold">Step 1 of 3</Text.Body2>*/}
+              {flow?.isCompleted || !flow ? (
+                <div className="flex items-center justify-center w-full h-full">
+                  <Spinner />
+                </div>
+              ) : (
+                <Frigade.Form
+                  flowId={ONBOARDING_FORM_FLOW_ID}
+                  dismissible={false}
+                  className="onboarding-form"
+                  css={{
+                    '.fr-button-primary': {
+                      width: '100%',
+                    },
+                    '.fr-field-label': {
+                      fontSize: '14px',
+                      marginBottom: '14px',
+                      lineHeight: '22px',
+                    },
+                    '.fr-field-check-value, .fr-field-radio-value': {
+                      width: '20px',
+                      height: '20px',
+                    },
+                    '.fr-field-check-label, .fr-field-radio-label': {
+                      lineHeight: '20px',
+                    },
+                    '.fr-field-input, .fr-field-select': {
+                      height: '38px',
+                    },
+                    '.fr-field-radio:hover, .fr-field-check:hover': {
+                      borderColor: 'var(--fr-colors-primary-background)',
+                    },
+                    '.fr-field-label-required': {
+                      display: 'none',
+                    },
+                    '.fr-field-select-icon': {
+                      color: 'var(--fr-colors-neutral-foreground)',
+                    },
+                    '.fr-field-radio': {
+                      cursor: 'pointer',
+                    },
+                    '.fr-field-check, .fr-field-radio': {
+                      height: '38px',
+                    },
+                    // FIXME/HELPME: @micah for some reason this doesn't work.
+                    // '.fr-field-select-option:hover': {
+                    //   backgroundColor: 'var(--fr-colors-secondary-hover-background)',
+                    // },
+                  }}
+                />
+              )}
+            </div>
+            <style>
+              {`
+                .fr-field-select-option:hover {
+                  background-color: var(--fr-colors-secondary-hover-background);
+                }
+                .fr-field-select-option {
+                  height: 38px;
+                }
+                .fr-field-select-option-label {
+                  display: flex;
+                  line-height: 22px;
+                }
+              `}
+            </style>
+          </Card>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/components/spinner.tsx
@@ -0,0 +1,85 @@
+import { cn } from '@/lib/utils';
+
+export function Spinner({ ...props }) {
+  return (
+    <div
+      className={cn(
+        'w-full h-full items-center justify-center flex',
+        props?.className,
+      )}
+    >
+      <svg className="new-spinner" viewBox="0 0 40 40" height="40" width="40">
+        <circle
+          className="track"
+          cx="20"
+          cy="20"
+          r="17.5"
+          pathLength="100"
+          strokeWidth="5px"
+          fill="none"
+        />
+        <circle
+          className="car"
+          cx="20"
+          cy="20"
+          r="17.5"
+          pathLength="100"
+          strokeWidth="5px"
+          fill="none"
+        />
+      </svg>
+      <style>
+        {`.new-spinner {
+        --uib-size: 40px;
+        --uib-color: var(--fr-colors-primary-background);
+        --uib-speed: 2s;
+        --uib-bg-opacity: 0;
+        height: var(--uib-size);
+        width: var(--uib-size);
+        transform-origin: center;
+        animation: rotate var(--uib-speed) linear infinite;
+        will-change: transform;
+        overflow: visible;
+      }
+
+        .car {
+        fill: none;
+        stroke: var(--uib-color);
+        stroke-dasharray: 1, 200;
+        stroke-dashoffset: 0;
+        stroke-linecap: round;
+        animation: stretch calc(var(--uib-speed) * 0.75) ease-in-out infinite;
+        will-change: stroke-dasharray, stroke-dashoffset;
+        transition: stroke 0.5s ease;
+      }
+
+        .track {
+        fill: none;
+        stroke: var(--uib-color);
+        opacity: var(--uib-bg-opacity);
+        transition: stroke 0.5s ease;
+      }
+
+        @keyframes rotate {
+        100% {
+          transform: rotate(360deg);
+      }
+      }
+
+        @keyframes stretch {
+        0% {
+          stroke-dasharray: 0, 150;
+        stroke-dashoffset: 0;
+      }
+        50% {
+        stroke-dasharray: 75, 150;
+        stroke-dashoffset: -25;
+      }
+        100% {
+        stroke-dashoffset: -100;
+      }
+      }`}
+      </style>
+    </div>
+  );
+}
