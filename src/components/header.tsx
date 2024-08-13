9ac1b3c715cff33957f0abec6d9551f382b5fb6e
Eric Brownrout <ericbrownrout@gmail.com>
2024-08-13 15:14:42 -0700

    More structure and styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import {Badge} from "@/components/ui/badge"
 import {Button} from "@/components/ui/button"
+import Link from "next/link";
 
 
 export function Header() {
   return (
     <div className="w-full bg-white border-b border-[#E5E5E5] flex justify-center">
-      <div className="z-10 w-full max-w-7xl items-center justify-between p-6 lg:px-0 text-sm flex">
+      <div className="z-10 w-full max-w-7xl items-center justify-between p-6 xl:px-0 text-sm flex">
         <div className="flex items-center dark:via-black static size-auto gap-2">
           <img src="/images/frigade.svg"></img>
-          <Badge variant="outline" className="px-2 py-0.5 mt-0.5">
-            <span className='text-[10px]'>Demo</span>
+          <Badge className="px-2 py-0.5 mt-0.5 bg-blue-600 border-none">
+            <span className='text-[9px]'>Demo</span>
           </Badge>
         </div>
 
         <div className="flex items-center dark:via-black static size-auto gap-6">
-          <a href="https://frigade.com" target="_blank" className="m-0 max-w-[30ch] lg:display text-sm font-medium hover:text-blue-500">Website</a>
-          <a href="https://frigade.com" target="_blank" className="m-0 max-w-[30ch] hidden md:block text-sm font-medium hover:text-blue-500">Docs</a>
-          <a href="https://frigade.com" target="_blank" className="m-0 max-w-[30ch] hidden md:block text-sm font-medium hover:text-blue-500">Github</a>
-          <Button size="sm" className='bg-blue-500'>Get Started</Button>
+          <Link href="https://frigade.com?ref=demo" target="_blank" className="m-0 max-w-[30ch] lg:display text-sm font-medium hover:text-blue-500">Website</Link>
+          <Link href="https://docs.frigade.com?ref=demo" target="_blank" className="m-0 max-w-[30ch] hidden md:block text-sm font-medium hover:text-blue-500">Docs</Link>
+          {/*TODO Add Github link*/}
+          <Link href="https://github.com" target="_blank" className="m-0 max-w-[30ch] hidden md:block text-sm font-medium hover:text-blue-500">Github</Link>
+          {/*TODO Add Github link*/}
+          <Button size="sm" className="bg-blue-600 hover:bg-blue-700">
+            <Link href="https://app.frigade.com/sign-up?ref=demo" target='_blank'>Get Started</Link>
+          </Button>
         </div>
       </div>
     </div>
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { ThemeDropdown } from '@/components/theme';
 
 export function Main({ children }: { children: React.ReactNode }) {
   return (
-    <div className='flex w-full h-full grow'>
-      <div className='max-w-7xl bg-white rounded-sm w-full justify-center items-center border flex mx-auto p-6'>
+    <div className="flex w-full h-full grow">
+      <div className="relative max-w-7xl shadow-sm bg-white rounded-lg w-full justify-center items-center border flex mx-auto p-6">
+        <div className="absolute left-6 top-6">
+          <ThemeDropdown />
+        </div>
         {children}
       </div>
     </div>
