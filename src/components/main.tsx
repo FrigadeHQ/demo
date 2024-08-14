a9badaee51e4441ec5aaff7e0cb35269b984dfeb
Eric Brownrout <ericbrownrout@gmail.com>
2024-08-13 18:03:27 -0700

    more form styles

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -6,7 +6,7 @@ import { ThemeDropdown } from '@/components/theme';
 export function Main({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex w-full grow">
-      <div className="relative max-w-7xl shadow-sm bg-white rounded-lg w-full justify-center items-center border flex mx-auto p-6">
+      <div className="relative max-w-7xl shadow-sm bg-background rounded-lg w-full justify-center items-center border flex mx-auto p-6">
         <div className="absolute left-6 top-6 z-10">
           <ThemeDropdown />
         </div>
