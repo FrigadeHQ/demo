129e7d93bc1d87e86fd9564b6f6659ff25bad676
Eric Brownrout <ericbrownrout@gmail.com>
2024-08-15 18:34:50 -0700

    tweak copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center text-center gap-2">
       <h2 className="font-medium">Welcome to the Frigade demo</h2>
-      <p className="text-sm text-muted-foreground max-w-[500px]">
-        We put together a collection of components and patterns to help you get
-        started. Feel free to grab time if you have any questions.
+      <p className="text-sm text-muted-foreground max-w-[400px]">
+        We put together a collection of our UI components and patterns to give
+        you a sense of what&apos;s possible.
       </p>
       <div className="flex flex-row gap-4 justify-center mt-6">
         <Button asChild>
