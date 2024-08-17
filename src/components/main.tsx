67d856ea86e614604a0d59b0c0b7429ce7288206
christianmat <christian_mathiesen@alumni.brown.edu>
2024-08-16 17:55:37 -0700

    analytics v2

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,12 +1,14 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ThemeDropdown } from '@/components/theme';
 import { Box } from '@frigade/react';
+import HeadContent from '@/components/head-content';
 
 export function Main({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <HeadContent />
       <div className="flex w-full grow px-6 xl:px-0 lg:hidden">
         <Box className="bg-grid -z-1"></Box>
         <div className="relative max-w-7xl shadow-sm bg-background rounded-lg w-full justify-center items-center border flex mx-auto p-6">
@@ -24,6 +26,42 @@ export function Main({ children }: { children: React.ReactNode }) {
           {children}
         </div>
       </div>
+      <Scripts />
     </>
   );
+}
+
+function Scripts() {
+  const [domLoaded, setDomLoaded] = useState(false);
+
+  useEffect(() => {
+    setDomLoaded(true);
+  }, []);
+
+  if (domLoaded) {
+    return (
+      <>
+        <script>
+          {`!function(t){if(window.ko)return;window.ko=[],["identify","track","removeListeners","open","on","off","qualify","ready"].forEach(function(t){ko[t]=function(){var n=[].slice.call(arguments);return n.unshift(t),ko.push(n),ko}});var n=document.createElement("script");n.async=!0,n.setAttribute("src","https://cdn.getkoala.com/v1/pk_211a1bb6ecee2b11dbe259ec95f3f88a66f1/sdk.js"),(document.body || document.head).appendChild(n)}();`}
+        </script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-WL6B5EKN4K"
+        ></script>
+        <script
+          src="https://www.googletagmanager.com/gtag/js?id=G-WL6B5EKN4K"
+          async
+        ></script>
+        <script>
+          {`  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WL6B5EKN4K');`}
+        </script>
+      </>
+    );
+  }
+
+  return null;
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,24 +20,6 @@ function MyApp({ Component, pageProps }: AppProps) {
           </Main>
           <Footer />
         </main>
-        <script>
-          {`!function(t){if(window.ko)return;window.ko=[],["identify","track","removeListeners","open","on","off","qualify","ready"].forEach(function(t){ko[t]=function(){var n=[].slice.call(arguments);return n.unshift(t),ko.push(n),ko}});var n=document.createElement("script");n.async=!0,n.setAttribute("src","https://cdn.getkoala.com/v1/pk_211a1bb6ecee2b11dbe259ec95f3f88a66f1/sdk.js"),(document.body || document.head).appendChild(n)}();`}
-        </script>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-WL6B5EKN4K"
-        ></script>
-        <script
-          src="https://www.googletagmanager.com/gtag/js?id=G-WL6B5EKN4K"
-          async
-        ></script>
-        <script>
-          {`  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-WL6B5EKN4K');`}
-        </script>
       </Providers>
     </>
   );
