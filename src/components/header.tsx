5d396664ddcc0ff59a8b01aee53143a7090bd125
Eric Brownrout <ericbrownrout@gmail.com>
2025-10-21 14:41:00 -0700

    add a Frigade AI link to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -18,7 +18,12 @@ export function Header() {
         </Link>
 
         <div className="flex items-center dark:via-black static size-auto gap-6">
-          <HeaderLink href={'https://frigade.com?ref=demo'}>Website</HeaderLink>
+          <HeaderLink href={'https://frigade.com?ref=demo'}>
+            Frigade.com
+          </HeaderLink>
+          <HeaderLink href={'https://frigade.ai?ref=demo'}>
+            Frigade.ai
+          </HeaderLink>
           <HeaderLink href={'https://docs.frigade.com?ref=demo'}>
             Docs
           </HeaderLink>
