5a6157fe84db3b254791b703ef42af019c79995c
Eric Brownrout <ericbrownrout@gmail.com>
2026-04-25 00:57:42 -0700

    feat(theme): adopt Inter Variable as the default body font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '@/pages/globals.css';
 import { Providers } from '@/components/providers';
-
+import { Inter } from 'next/font/google';
 import { AppProps } from 'next/app';
 import { Header } from '@/components/header';
 import { Main } from '@/components/main';
@@ -8,9 +8,20 @@ import { Footer } from '@/components/footer';
 
 // import HeadContent from '@/components/layout/head-content';
 
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-sans',
+  display: 'swap',
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <style jsx global>{`
+        :root {
+          --font-sans: ${inter.style.fontFamily};
+        }
+      `}</style>
       <Providers>
         {/*<HeadContent />*/}
         <main className="flex bg-[#F6F8F7] min-h-screen flex-col items-center justify-between p-0 gap-8">
